7d0e68752afc111c173bf9ad59cf90151bbbf447
Mauritius Loic <123571057+LoicMauritius@users.noreply.github.com>
2024-01-26 19:15:28 +0100

    Files from fr/ and doc/

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"fr.but.Mauritius_Loic_Booms_Amaury.Game"}];updateSearchResults();
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"fr.but.Mauritius_Loic_Booms_Amaury.Game","l":"GameController"}];updateSearchResults();
